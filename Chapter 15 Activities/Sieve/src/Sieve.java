60a5bc97c456ab23f31fd2d0f937ed40549ebec6
addison-cho <111458978+addison-cho@users.noreply.github.com>
2023-09-25 12:51:41 -0500

    .

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A program that implements the sieve of Eratosthenes.
@@ -13,11 +15,32 @@ public class Sieve
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        
 
+        Set<Integer> nums = new HashSet<>();
+        int i = 2;
+        while(i<=n){
+            nums.add(i);
+            i++;
+        }
+        i = 2;
+        
+            Iterator iterator = nums.iterator();
 
+        while(i<=n){
+            while(iterator.hasNext()){
+                int x = (int) iterator.next();
+                if (x % i == 0 && x != i) {
+                    iterator.remove();
+                }
+            }
+                i++;
+                iterator = nums.iterator();            
 
+            }
 
+        System.out.println(nums);
+            System.out.println("k");
 
 
 
