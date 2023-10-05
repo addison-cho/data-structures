b0048539d5dbb5dcf2118157326ed46e3408b396
addison-cho <111458978+addison-cho@users.noreply.github.com>
2023-10-05 13:25:42 -0500

    .

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,46 @@
+import java.util.Stack;
+
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<int[]> fill = new Stack<>();
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        int[] coord = new int[]{row, column};
+        fill.push(coord);
+
+        int count = 1;
+        while(!fill.isEmpty()) {
+            coord = fill.pop();
+            row = coord[0];
+            int col = coord[1];
+
+            if (pixels[row][col] == 0) {
+                if (row != 0) {
+                    fill.push(new int[]{row-1, col});
+                }
+                if (col != 9) {
+                    fill.push(new int[]{row, col+1});
+                }
+                if (row != 9) {
+                    fill.push(new int[]{row+1, col});
+                }
+                if (col != 0) {
+                    fill.push(new int[]{row, col-1});
+                }
+
+                pixels[row][col] = count;
+                count++;
+            }
+        }
+
+        System.out.println(toString());
+        
     }
 
     @Override
