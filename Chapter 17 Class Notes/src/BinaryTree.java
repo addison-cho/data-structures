8378622959da03ab05fed7736cd0516245a2154a
addison-cho <111458978+addison-cho@users.noreply.github.com>
2023-11-13 13:03:36 -0600

    .

--- Chapter 17 Class Notes/src/BinaryTree.java
+++ Chapter 17 Class Notes/src/BinaryTree.java
@@ -10,7 +10,7 @@ public class BinaryTree
     */
     public BinaryTree()
     {
-         
+         this.root = null;
     } 
 
     /**
@@ -19,7 +19,10 @@ public class BinaryTree
     */
     public BinaryTree(Object rootData) 
     {
-        
+        this.root = new Node();
+        this.root.data = rootData;
+        this.root.left = null;
+        this.root.right = null;
     }
 
     /**
@@ -30,12 +33,15 @@ public class BinaryTree
     */
     public BinaryTree(Object rootData, BinaryTree left, BinaryTree right)
     {
-        
+        this(rootData);
+        this.root.left = left.root;
+        this.root.right = right.root;
     }
     
     static class Node
     {
-        
+        public Object data;
+        public Node left, right;
     }
 
     /**
@@ -45,7 +51,9 @@ public class BinaryTree
     */
     private static int height(Node n)
     {
-        return 0;
+        if (n == null) return 0;
+
+        return 1 + Math.max(BinaryTree.height(n.left), BinaryTree.height(n.right));
     }
 
     /**
@@ -54,7 +62,7 @@ public class BinaryTree
     */
     public int height()
     {
-        return 0;
+        return BinaryTree.height(this.root);
     }
 
     /**
@@ -63,7 +71,7 @@ public class BinaryTree
     */
     public boolean isEmpty()
     {
-         return false;
+         return this.root == null;
     }
 
     /**
@@ -72,7 +80,7 @@ public class BinaryTree
     */
     public Object data()
     {
-         return null;
+         return root.data;
     }
     
     /**
@@ -81,7 +89,9 @@ public class BinaryTree
     */
     public BinaryTree left() 
     { 
-        return null;
+        BinaryTree subtree = new BinaryTree();
+        subtree.root = this.root.left;
+        return subtree;
     }
 
     /**
@@ -90,6 +100,8 @@ public class BinaryTree
     */
     public BinaryTree right() 
     { 
-        return null;
+        BinaryTree subtree = new BinaryTree();
+        subtree.root = this.root.right;
+        return subtree;
     }
 }
